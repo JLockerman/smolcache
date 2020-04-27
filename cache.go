22d5b78f92999ba96f53a9226301aee39f097681
Joshua Lockerman <>
2020-04-27 15:49:29 -0400

    Switch to a flat layout for Elements
    This commit switched the cache to using a flat layout for Elements;
    instead of storing the Elements in a linked list, it stores them in a
    slice, and stores indexes into the slice where it once stored pointers.
    This has two advantages:
    
      1. The resulting code is slightly smaller.
      2. It has better locality
    
    One issue the linked list version could have is that after extensive
    deletions and additions, the Elements could be scattered across memory.
    By storing the elements in slices, and reusing the slots when elements
    are removed, all the elements should remain close together.
    
    This strategy has the disadvantage in that it might make eviction
    slower; it depends on if the better locality or the number of elements
    to check is the dominant term.

--- cache.go
+++ cache.go
@@ -35,22 +35,42 @@ type Interner struct {
 
 type block struct {
 	lock sync.RWMutex
-	// guarded by lock
-	elements map[string]*Element
+	// guarded by lock, indexes into elements
+	mapping map[string]int
 
-	// only safe to not use a pointer since blocks never move
-	sweep List
+	elements []Element
+
+	//index into elements
+	first_free_element int
 
 	// CLOCK sweep state, guarded by clockLock
-	next *Element
+	next int //index into elements
 	// pad blocks out to be cache aligned
-	_padding [16]byte
+	_padding [56]byte
+}
+
+type Element struct {
+	// The value stored with this element.
+	key   string
+	Value int64
+
+	// index of this element in elements
+	idx int
+
+	// when this is on the freelist, the index of the next free element
+	next_free int
+
+	//CLOCK marker if this is recently used
+	used uint32
+
+	_padding [16]uint8
 }
 
 func WithMax(max uint64) *Interner {
 	return &Interner{
 		max:  max,
 		seed: maphash.MakeSeed(),
+		maps: [127]block{block{first_free_element: -1}},
 	}
 }
 
@@ -72,15 +92,31 @@ func (i *Interner) Insert(key string, value int64) {
 func (b *block) insert(key string, value int64) bool {
 	b.lock.Lock()
 	defer b.lock.Unlock()
-	if b.elements == nil {
-		b.elements = make(map[string]*Element)
+	if b.mapping == nil {
+		b.mapping = make(map[string]int)
 	}
-	_, present := b.elements[key]
+	_, present := b.mapping[key]
 	if present {
 		return false
 	}
-	elem := b.sweep.PushBack(key, value)
-	b.elements[key] = elem
+	var idx int
+	if b.first_free_element > 0 {
+		element := &b.elements[b.first_free_element]
+		b.first_free_element = element.next_free
+		element.next_free = -1
+		element.key = key
+		element.Value = value
+		element.used = 0
+		idx = element.idx
+	} else {
+		idx = len(b.elements)
+		b.elements = append(b.elements, Element{
+			key:   key,
+			Value: value,
+			idx:   idx,
+		})
+	}
+	b.mapping[key] = idx
 	return true
 }
 
@@ -106,9 +142,9 @@ func (i *Interner) evict() {
 func (b *block) tryEvict() (evicted bool, reachedEnd bool) {
 	b.lock.Lock()
 	defer b.lock.Unlock()
-	if b.next == nil {
-		b.next = b.sweep.Front()
-		if b.next == nil {
+	if b.next == len(b.elements) {
+		b.next = 0
+		if b.next == len(b.elements) {
 			return false, true
 		}
 	}
@@ -116,14 +152,18 @@ func (b *block) tryEvict() (evicted bool, reachedEnd bool) {
 	evicted = false
 	reachedEnd = false
 	for !evicted && !reachedEnd {
-		elem := b.next
-		b.next = elem.Next()
-		reachedEnd = b.next == nil
-		if elem.used != 0 {
+		idx := b.next
+		b.next = idx + 1
+		reachedEnd = b.next == len(b.elements)
+		elem := &b.elements[idx]
+		if elem.used == 1 {
 			elem.used = 0
-		} else {
-			key, _ := b.sweep.Remove(elem)
-			delete(b.elements, key)
+		} else if elem.used == 0 {
+			elem.used = 2
+			delete(b.mapping, elem.key)
+			elem.Value = 0
+			elem.next_free = b.first_free_element
+			b.first_free_element = idx
 			evicted = true
 		}
 	}
@@ -143,13 +183,14 @@ func (i *Interner) Get(key string) (int64, bool) {
 func (b *block) get(key string) (int64, bool) {
 	b.lock.RLock()
 	defer b.lock.RUnlock()
-	if b.elements == nil {
+	if b.mapping == nil {
 		return 0, false
 	}
-	elem, present := b.elements[key]
+	idx, present := b.mapping[key]
 	if !present {
 		return 0, false
 	}
+	elem := &b.elements[idx]
 
 	if atomic.LoadUint32(&elem.used) == 0 {
 		atomic.StoreUint32(&elem.used, 1)
@@ -170,13 +211,14 @@ func (i *Interner) Unmark(key string) bool {
 func (b *block) unmark(key string) bool {
 	b.lock.RLock()
 	defer b.lock.RUnlock()
-	if b.elements == nil {
+	if b.mapping == nil {
 		return false
 	}
-	elem, present := b.elements[key]
+	idx, present := b.mapping[key]
 	if !present {
 		return false
 	}
+	elem := &b.elements[idx]
 
 	if atomic.LoadUint32(&elem.used) != 0 {
 		atomic.StoreUint32(&elem.used, 0)
